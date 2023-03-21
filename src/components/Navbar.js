b5a781125f2eb9bcb67c2ff317723532ba2a590e
Jyoti Singh <jyotisingh82000@gmail.com>
2023-03-21 16:29:01 +0530

    minor change

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react"
+import logo1 from "../assets/logo2.png"
+import "./Navbar.css"
+
+const Navbar = () => {
+
+    return (
+
+        <div id="box" >
+            <div className="">
+
+                <img src={logo1} className="w-70 border-4 rounded-2xl border-zinc-50" alt="" />
+            </div>
+        </div>
+
+
+    )
+}
+export default Navbar;
