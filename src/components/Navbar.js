64af782e26cf98ec6129447b5cb082995aa6f730
Jyoti Singh <jyotisingh82000@gmail.com>
2023-03-24 03:54:50 +0530

    minor change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,30 @@
 import React from "react"
 import logo1 from "../assets/logo2.png"
+import profile from "../assets/profile.png"
 import "./Navbar.css"
 
 const Navbar = () => {
 
     return (
 
-        <div id="box" >
-            <div className="">
+        <div id="box" className="backdrop-opacity-10~" >
+            <div className=" flex justify-evenly">
 
-                <img src={logo1} className="w-70 border-4 rounded-2xl border-zinc-50" alt="" />
+                <img src={logo1} className=" rounded-2xl border-zinc-50" alt="" />
+                <img src={profile} className="p-2 rounded-2xl border-zinc-50" alt="" />
+            </div>
+            <div>
+                <div>
+                    <button>Home</button><button>Category</button>
+
+                </div>
+                <div>
+                    <button>Cart</button><button>Order</button>
+                </div>
+            </div>
+            <div>
+                <input type="text" />
+                <img src="" alt="" />
             </div>
         </div>
 
